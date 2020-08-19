4f357f61d23f5275080cb9badfaa02c9ee3d68c4
arily <lbsaaaaa@gmail.com>
2020-08-20 04:47:57 +0900

    create sessionStore

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ class SessionContainer {
     }
 
     async cacheAndReturnDbSession(session) {
-        const _session = await this.dbGetSession(session)
+        let _session = await this.dbGetSession(session)
+        _session = createSessionStore(session, this)
         // _session.user = await this.UserModel.findOne(_session.user).exec()
         // console.log(_session)
         // if (!this.getSession(_session)) 
