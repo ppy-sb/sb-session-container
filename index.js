99e4d97c1de5641be019257422357fb2e736325d
arily <lbsaaaaa@gmail.com>
2020-08-20 06:27:40 +0900

    update

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const APP_NAMESPACE = '9f4750c8-c940-11ea-87d0-0242ac130003'
 
 const createSessionStore = require('./instance/SessionStore')
 const NoSessionError = require('./errors/NoSessionError')
+
+/**
+ * SessionContainer
+ * @author arily
+ */
 class SessionContainer {
+    /**
+     * Create SessionContaner instance
+     * @param {sb-db} database 
+     */
     constructor({ Session, User }) {
         this.sessions = new Map
         this.SessionModel = Session
@@ -13,10 +22,28 @@ class SessionContainer {
             .forEach((method) => (this[method] = this[method].bind(this)))
     }
 
+    /**
+     * get session
+     * @param {SessionStore | Object} session - An Object with id key
+     * @returns {SessionStore | undefined} session
+     */
     async getSession({ id }) {
         return this.sessions.get(id) || this.cacheAndReturnDbSession({ id })
     }
 
+    /**
+    * alias of SessionContainer.destroy
+    * @param {SessionStore | Object} session - An Object with id key
+    */
+    quit(session) {
+        this.destroy(session)
+    }
+
+    /**
+    * retrieve session and store in sessions
+    * @param {SessionStore | Object} session - An Object with id key
+    * @returns {SessionStore} session
+    */
     async cacheAndReturnDbSession(session) {
         let _session = await this.dbGetSession(session)
         _session = createSessionStore(_session, this)
@@ -28,63 +55,31 @@ class SessionContainer {
         return _session
     }
 
+    /**
+     * get session from db
+     * @param {SessionStore | Object} session - An Object with id key
+     * @returns {Object} session data
+     */
     dbGetSession({ id }) {
-        return this.SessionModel.findOne({ id }).exec().then(res => res.toObject())
+        return this.SessionModel.findOne({ id }).exec().then(res => {
+            if (!res) return res
+            return res.toObject()
+        })
     }
 
+    /**
+     * destroy a session
+     * will not destroy the session in database
+     * @param {SessionStore | Object} session 
+     */
     destroy(session) {
         this.sessions.delete(session.id)
     }
 
-    async start(req, session) {
-        session.id = uuidv4()
-        session = createSessionStore(session, this)
-        console.info(`Session for ${session.user.name} created, session(${session.id})`)
-        this.sessions.set(session.id, session)
-        req.token = session.id
-        session.onUpdate()
-        await this.debounceSession(req)
-    }
-
-    fromToken(req, res, next) {
-        req.token = req.header('osu-token')
-        next()
-    }
-
-    async continue(req, res, next) {
-        if (!req.token) return next('route') // @arily next('route')和next()不同 同一条路由后面的中间件会被跳过.
-        const session = await this.getSession({ id: req.token })
-        if (!session) return next(new NoSessionError())
-        if (!session.user) return next(new NoSessionError('user undefined'))
-        await this.debounceSession(req)
-        console.info(`Session for ${req.session.user.name} resumed, session(${req.session.id})`)
-        next()
-    }
-
-    quit(session) {
-        this.destroy(session)
-    }
-
-    nextRouteIfNoSession(req, res, next) {
-        if (!req.session) return next("route")
-        next()
-    }
-
-    nextRouteIfNotResumedSession(req, res, next) {
-        if (!req.session.resumed) return next("route")
-        next()
-    }
-
-    removeResumedSessionFlag(req, res, next) {
-        delete req.session.resumed
-        next()
-    }
-
-    nextRouteIfHasSession(req, res, next) {
-        if (req.session) return next("route")
-        next()
-    }
-
+    /**
+     * handling exipre for session and bond session to request
+     * @param {Request} req - Express Request
+     */
     async debounceSession(req) {
         let timer
         const session = await this.getSession({ id: req.token })
@@ -102,6 +97,96 @@ class SessionContainer {
                 return session
             },
         })
+    }
+
+    /* ==== */
+
+    /**
+     * start a session
+     * @param {Request} req - Express Request
+     * @param {Object} session - initial state of session
+     */
+    async start(req, session) {
+        session.id = uuidv4()
+        session = createSessionStore(session, this)
+        console.info(`Session for ${session.user.name} created, session(${session.id})`)
+        this.sessions.set(session.id, session)
+        req.token = session.id
+        session.onUpdate()
+        await this.debounceSession(req)
+    }
+
+    /* ==== */
+
+    /**
+     * mark token from request header
+     * express middleware
+     * @param {Request} req Express Request
+     * @param {Response | Any} res Express Response (unused)
+     * @param {next} next Express next()
+     */
+    fromToken(req, res, next) {
+        req.token = req.header('osu-token')
+        next()
+    }
+
+    /**
+     * bond a session to request if a session exists
+     * @param {Request} req Express Request
+     * @param {Response | Any} res Express Response (unused)
+     * @param {next} next Express next()
+     */
+    async continue(req, res, next) {
+        if (!req.token) return next('route') // @arily next('route')和next()不同 同一条路由后面的中间件会被跳过.
+        const session = await this.getSession({ id: req.token })
+        if (!session) return next(new NoSessionError())
+        if (!session.user) return next(new NoSessionError('user undefined'))
+        await this.debounceSession(req)
+        console.info(`Session for ${req.session.user.name} resumed, session(${req.session.id})`)
+        next()
+    }
+
+    /**
+     * jump to next route if no session is bonded with the request
+     * @param {Request} req Express Request
+     * @param {Response | Any} res Express Response (unused)
+     * @param {next} next Express next()
+     */
+    nextRouteIfNoSession(req, res, next) {
+        if (!req.session) return next("route")
+        next()
+    }
+    /**
+    * jump to next route if a is bonded with the request
+    * @param {Request} req Express Request
+    * @param {Response | Any} res Express Response (unused)
+    * @param {next} next Express next()
+    */
+    nextRouteIfHasSession(req, res, next) {
+        if (req.session) return next("route")
+        next()
+    }
+
+    /**
+    * jump to next route if resumed flag of session is not true
+    * @param {Request} req Express Request
+    * @param {Response | Any} res Express Response (unused)
+    * @param {next} next Express next()
+    */
+    nextRouteIfNotResumedSession(req, res, next) {
+        if (!req.session.resumed) return next("route")
+        next()
+    }
+
+    /**
+    * clear resumed flag from request.session
+    * @param {Request} req Express Request
+    * @param {Response | Any} res Express Response (unused)
+    * @param {next} next Express next()
+    */
+    removeResumedSessionFlag(req, res, next) {
+        delete req.session.resumed
+        next()
     }
 
     handleSessionError(err, req, res, next) {
--- instance/SessionStore.js
+++ instance/SessionStore.js
@@ -1,25 +1,47 @@
-// const db = require('../../controllers/db')
-function Store(container) {
+/**
+ * SessionStore
+ * @constructor
+ * @param {SessionContainer} container SessionContainer
+ */
+function SessionStore(container) {
     this.pendingPackets = []
     this.container = container
 }
 
+/**
+ * return and reset pending packets
+ * @returns {Array} packets
+ */
 Store.prototype.flushPendingPackets = function () {
     const packets = [...this.pendingPackets]
     this.pendingPackets.length = 0
     return packets
 }
 
+/**
+ * callback when session is updated
+ */
 Store.prototype.onUpdate = function () {
     const SessionModel = this.container.SessionModel
     setImmediate(() => SessionModel.findOneAndUpdate({ id: this.id }, this, { upsert: true }, err => { if (err) console.warn(err) }))
     console.info(`Cloud session for ${this.user.name} updated, session(${this.id})`)
 }
 
+/**
+ * shortcut for destroy session
+ */
 Store.prototype.quit = function () {
     this.container.quit(this)
 }
 
+/**
+ * Builder function for SessionStore
+ * @module SessionStore
+ * @see:SessionStore
+ * @param {Object} obj initial state for SessionStore
+ * @param {SessionContainer} container SessionContainer
+ * @returns {SessionStore}
+ */
 module.exports = (obj, container) => {
-    return Object.assign(new Store(container), obj)
+    return Object.assign(new SessionStore(container), obj)
 }
