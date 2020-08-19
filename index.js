af25e7df07244193715d6bc5bbdb3baad2f19ef9
arily <lbsaaaaa@gmail.com>
2020-08-20 04:36:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ class SessionContainer {
     async cacheAndReturnDbSession(session) {
         const _session = await this.dbGetSession(session)
         _session.user = await this.UserModel.findOne(_session.user).exec()
+        console.log(_session)
         // if (!this.getSession(_session)) 
         this.sessions.set(session.id, session)
         return session
